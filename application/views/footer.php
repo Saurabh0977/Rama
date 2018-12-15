04141ac40b7e75d75bafeb431b9b1129db11e1d6
unbeatable <unbeatable@unbeatables-MacBook-Air.local>
2018-12-15 22:05:54 +0530

    footer

--- application/views/footer.php
+++ application/views/footer.php
@@ -1,18 +1,18 @@
 <!-- Footer -->
-			<div class="navbar navbar-expand-lg navbar-light">
-				<div class="text-center d-lg-none w-100">
-					<button type="button" class="navbar-toggler dropdown-toggle" data-toggle="collapse" data-target="#navbar-footer">
-						<i class="icon-unfold mr-2"></i>
-						Footer
-					</button>
-				</div>
+<div class="navbar navbar-expand-lg navbar-light">
+    <div class="text-center d-lg-none w-100">
+        <button type="button" class="navbar-toggler dropdown-toggle" data-toggle="collapse" data-target="#navbar-footer">
+            <i class="icon-unfold mr-2"></i>
+            Footer
+        </button>
+    </div>
 
-				<div class="navbar-collapse collapse" id="navbar-footer">
-					<span class="navbar-text">
-						&copy;2018. <a>Web App</a> by <a href="http://www.dreampool.in" target="_blank">Dreampool</a>
-					</span>
+    <div class="navbar-collapse collapse" id="navbar-footer">
+        <span class="navbar-text">
+            &copy;2018. <a>Web App</a> by <a href="#">Saurabh Kumar</a>
+        </span>
 
-					
-				</div>
-			</div>
-			<!-- /footer -->
+
+    </div>
+</div>
+<!-- /footer -->
